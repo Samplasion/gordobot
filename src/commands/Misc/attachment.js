9dd0337eacab9bda11d26e2a5cf2133e85113a4c
castyte <castyte@pm.me>
2019-10-17 19:30:51 +0100

    Update attachment.js

--- src/commands/Misc/attachment.js
+++ src/commands/Misc/attachment.js
@@ -3,20 +3,18 @@
  * See LICENCE file or <https://www.gnu.org/licenses/gpl-3.0.txt>
 */ 
 
-var Discord = require('discord.js')
-
 module.exports = {
   name: 'attachment',
   description: 'Returns the link for an attachment',
-  usage: '',
+  usage: '{{message attachment}}',
   cooldown: 5,
   aliases: ['attach', 'link'],
   access: 0,
-  guildOnly: false,
+  guildOnly: true,
 
-  execute(message,args) {
+  execute(message) {
     if (message.attachments && message.attachments.first()) {
-      message.reply(message.attachments.first().url)
+        message.reply(message.attachments.first().url)
     } else message.reply("You didn't attach anything")
   },
 };
